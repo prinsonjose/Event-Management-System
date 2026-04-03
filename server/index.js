2005c6fb6add283ec0d6a0c41bf8c9d895064abd
prinsonjose <joseprinson3@gmail.com>
2026-04-03 14:43:35 +0530

    Fixed CORS issue

--- server/index.js
+++ server/index.js
@@ -10,14 +10,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    'https://event-management-frontend-fb43.onrender.com',
-    'http://localhost:5173'
-  ],
-  credentials: true,
-}));
+const cors = require("cors");
 
+app.use(cors({
+  origin: "https://event-management-frontend-fb43.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
